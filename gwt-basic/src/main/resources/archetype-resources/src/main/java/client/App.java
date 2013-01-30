770c150105521c386ea8eba95afec7608a1eda2e
madz <markdiesta@gmail.com>
2013-01-30 21:35:15 +0800

    update README update App.java update pom.xml update archetype-catalog.xml

--- gwt-basic/src/main/resources/archetype-resources/src/main/java/client/App.java
+++ gwt-basic/src/main/resources/archetype-resources/src/main/java/client/App.java
@@ -1,13 +1,27 @@
 package ${package}.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
 
 public class App implements EntryPoint {
 
 	public void onModuleLoad() {
-		Window.alert("Hello GWT!");
+		Button btnClickMe = new Button("Click Me!");
+		
+		btnClickMe.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				Window.alert("Hello GWT!");				
+			}
+		});
+		
+		RootPanel.get().add(btnClickMe);
 	}
 
 }
