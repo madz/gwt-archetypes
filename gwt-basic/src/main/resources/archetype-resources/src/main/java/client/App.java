52eaf37fb57fa3fd61c97021f43e2f45e3ba8f7a
madz <markdiesta@gmail.com>
2013-01-30 20:29:37 +0800

    first commit to gwt basic archetypes

---
+++ gwt-basic/src/main/resources/archetype-resources/src/main/java/client/App.java
@@ -0,0 +1,13 @@
+package ${package}.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+
+public class App implements EntryPoint {
+
+	public void onModuleLoad() {
+		Window.alert("Hello GWT!");
+	}
+
+}
---
+++ gwt-basic/src/main/resources/archetype-resources/src/main/java/server/Util.java
@@ -0,0 +1,5 @@
+package ${package}.server;
+
+public class Util {
+
+}
